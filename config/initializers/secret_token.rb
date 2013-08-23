d4b84c0ac64899bd34c572946824edf898ac0e50
Hassen Taidirt <htaidirt@gmail.com>
2013-08-24 00:15:32 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Copernic::Application.config.secret_key_base = 'ebf24f7961e80decbb47f39195f074a6b75ea0a938c41352a4acfeb55744ed844a8e7cb0665c3730121096e236bc83b9296e63237c0756bd923a0f552d82fd83'
